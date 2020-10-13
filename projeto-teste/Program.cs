d6b7243adb05fe07a97a4f952752039a3362e2e3
wenderson da silva schroder <wendersondasilva3@gmail.com>
2020-10-13 17:54:14 -0300

    Acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
+            Console.WriteLine("---------------------------");
         }
     }
 }
